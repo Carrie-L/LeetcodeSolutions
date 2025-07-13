d0325c96adac0196924767489df87d67dc516378
Carrie-L <11453154+Carrie-L@users.noreply.github.com>
2025-07-13 23:54:13 +0800

    feat: add array-topic wrapper for LC33 & remove symlink

--- src/main/kotlin/leetcode/array/LC33_SearchInRotatedSortedArray.kt
+++ src/main/kotlin/leetcode/array/LC33_SearchInRotatedSortedArray.kt
@@ -1,1 +1,8 @@
-../binary_search/LC33_SearchInRotatedSortedArray.kt
+package leetcode.array
+
+import leetcode.binary_search.Solution as BS
+
+/** Re-export LC33 under the 'array' topic */
+object Solution33 : (IntArray, Int) -> Int {
+    override fun invoke(nums: IntArray, target: Int) = BS.searchRotatedArray2(nums, target)
+}
