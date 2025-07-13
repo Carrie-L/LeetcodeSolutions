6b1dce250a3b631f88c0a34be0520edb093088e3
Carrie-L <982096112@qq.com>
2025-07-13 12:01:43 +0000

    [LeetCode Sync] Runtime - 11 ms (15.06%), Memory - 45.3 MB (29.00%)

--- leetcode_sync/0033-search-in-rotated-sorted-array/solution.kt
+++ leetcode_sync/0033-search-in-rotated-sorted-array/solution.kt
@@ -1,22 +1,14 @@
 class Solution {
     fun search(nums: IntArray, target: Int): Int {
         var low = 0
-        var high = nums.size - 1
+        var high = nums.lastIndex
         while (low <= high) {
-            val mid = low + (high - low) / 2
+            val mid = low + high ushr 1
             if (nums[mid] == target) return mid
-
-            if (nums[mid] >= nums[high]) {  // 说明 mid 在左边序列
-                if (nums[low] > target || nums[mid] < target) { // 完全不在左边序列
-                    low = mid + 1
-                } else {
-                    high = mid - 1
-                }
-            } else if (nums[mid] < nums[high]) { // 说明 mid 在右边序列
-                if (nums[high] < target || nums[mid] > target) high = mid - 1
-                else {
-                    low = mid + 1
-                }
+            if (nums[mid] >= nums[low]) {
+                if (target >= nums[low] && target < nums[mid]) high = mid - 1 else low = mid + 1
+            } else {
+                if (target > nums[mid] && target <= nums[high]) low = mid + 1 else high = mid - 1
             }
         }
         return -1
